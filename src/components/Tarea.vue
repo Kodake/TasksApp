8ead3c10549a7f271bb767caa25b3c48670211cf
Kodake <39713700+Kodake@users.noreply.github.com>
2020-12-30 02:10:13 -0400

    All changes about the integration with the backend

--- src/components/Tarea.vue
+++ src/components/Tarea.vue
@@ -17,17 +17,30 @@
                 <button @click="agregarTarea()" class="btn btn-success btn-lg">Agregar</button>
               </div>
             </div>
+
             <br />
+
+            <div class="text-center p-3" v-if="loading">
+              <div class="spinner-border text-primary" role="status">
+                <span class="sr-only">Loading...</span>
+              </div>
+              <div class="spinner-border text-success" role="status">
+                <span class="sr-only">Loading...</span>
+              </div>
+              <div class="spinner-border text-danger" role="status">
+                <span class="sr-only">Loading...</span>
+              </div>
+            </div>
 
             <h5 v-if="listTareas.length == 0">No hay tareas para realizar</h5>
 
-            <ul class="list-group">
+            <ul v-if="!loading" class="list-group">
               <li v-for="(tarea, index) of listTareas" :key="index" class="list-group-item d-flex justify-content-between">
-                <span class="cursor" v-bind:class="{'text-success' : tarea.estado}" v-on:click="editarTarea(tarea, index)">
+                <span class="cursor" v-bind:class="{'text-success' : tarea.estado}" v-on:click="editarTarea(tarea, tarea.id)">
                   <i v-bind:class="[tarea.estado ? 'fas fa-check-circle' : 'far fa-circle']"></i>
                 </span>
                 {{tarea.nombre}}
-                <span class="text-danger cursor" @click="eliminarTarea()">
+                <span class="text-danger cursor" @click="eliminarTarea(tarea.id)">
                   <i class="fas fa-trash-alt"></i>
                 </span>
               </li>
@@ -40,12 +53,15 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: "Tarea",
   data() {
     return {
       tarea: "",
       listTareas: [],
+      loading: false
     };
   },
   methods: {
@@ -54,15 +70,46 @@ export default {
               nombre: this.tarea,
               estado: false
           }
-          this.listTareas.push(tarea);
+          this.loading = true;
+          axios.post("https://localhost:44326/api/Tareas/", tarea).then(resp => {
+            this.loading = false;
+            this.obtenerTareas();
+            console.log(resp);
+          }).catch(err => {
+            this.loading = false;
+            console.error(err);
+          });
           this.tarea = '';
       },
-      eliminarTarea(index) {
-          this.listTareas.splice(index, 1);
+      editarTarea(tarea, id) {
+        /*  this.listTareas[index].estado = !tarea.estado; */
+        this.loading = true;
+        axios.put("https://localhost:44326/api/Tareas/"+ id, tarea).then(()=> {
+          this.obtenerTareas();
+          this.loading = false
+        }).catch(() => this.loading = false);
       },
-      editarTarea(tarea, index) {
-        this.listTareas[index].estado = !tarea.estado
+      eliminarTarea(id) {
+        this.loading = true;
+          axios.delete("https://localhost:44326/api/Tareas/" + id).then(resp => {
+            this.loading = false;
+            this.obtenerTareas();
+            console.log(resp);
+        }).catch(err => {
+          this.loading = false;
+          console.error(err);
+        });
+      },
+      obtenerTareas() {
+        this.loading = true;
+        axios.get("https://localhost:44326/api/Tareas").then(resp => {
+          this.listTareas = resp.data;
+          this.loading = false;
+        }).catch(() => this.loading = false);
       }
+  },
+  created: function() {
+    this.obtenerTareas();
   }
 };
 </script>
